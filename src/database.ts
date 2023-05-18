2ff90eeefc37b7e2a6c7a82349b5ad086aaf911c
Paul Mielle <pm.mielle@gmail.com>
2023-05-18 16:08:15 +0200

    use the mongo replica set

--- src/database.ts
+++ src/database.ts
@@ -5,12 +5,7 @@ import { Observable, from, switchMap } from "rxjs";
 
 dotenv.config({ path: ".env.dev" });
 
-const mongoUsername = process.env["MONGODB_USERNAME"];
-const mongoPassword = process.env["MONGODB_PASSWORD"];
-const mongoHost = process.env["MONGODB_HOST"];
-const mongoPort = process.env["MONGODB_PORT"];
-const mongoDatabase = process.env["MONGODB_DATABASE"];
-const uri = `mongodb://${mongoUsername}:${mongoPassword}@${mongoHost}:${mongoPort}/${mongoDatabase}`;
+const uri = process.env["MONGODB_CONNECTION_STRING"];
 
 const client = new MongoClient(uri);
 const db = client.db();
